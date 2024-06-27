900db027ef3aa5a16bd347d9a5ea65b708057a45
Amira Natasha Binti Abdullah Harun <v-ambintiabd@microsoft.com>
2024-06-27 06:24:48 +0000

    aa

--- ConsoleApp7/Class1.cs
+++ ConsoleApp7/Class1.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp7
 {
-    internal class Class1
+    internal class Class1//this class is internal so it can be accessed only within the same assembly
     {
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        public void Method2() {
+            Console.WriteLine("Method2");
+        }
     }
 }
